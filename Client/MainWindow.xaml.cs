37f23b4386e2d9f9ced2ee0ef97f0977a96cd951
Haqve_vu <novruzluqyds20@gmail.com>
2023-11-26 05:43:43 -0800

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket Client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        static IPAddress remote_ip = IPAddress.Parse("127.0.0.1");
+        static int remote_port = 4000;
+
+        EndPoint remote_endpoint = new IPEndPoint(remote_ip, remote_port);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            string message = "Send Screenshoot";
+
+            byte[] buffer = Encoding.Default.GetBytes(message);
+
+            Client.SendTo(buffer, remote_endpoint);
+
+            SetImageSource();
+        }
+
+        private async void SetImageSource()
+        {
+            try
+            {
+
+                await Task.Run(() =>
+                {
+                    var msg = "";
+                    var len = 0;
+                    var receive_buffer = new byte[1024];
+
+                    len = Client.ReceiveFrom(receive_buffer, ref remote_endpoint);
+
+                    msg = Encoding.Default.GetString(receive_buffer, 0, len);
+                    // Create a BitmapImage and set its UriSource
+                    Dispatcher.Invoke(() =>
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.UriSource = new Uri(msg);
+                        bitmapImage.EndInit();
+
+                        ScreenShoot.Source = bitmapImage;
+                    });
+                     
+                });
+
+                // Set the Image control's source
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, e.g., file not found
+                MessageBox.Show($"Error loading image: {ex.Message}");
+            }
+        }
+
+
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.IO;
+#pragma warning disable
+
+namespace Server
+{
+    public class Server
+    {
+
+        static void Main(string[] args)
+        {
+            string Path = @"C:\Users\Elgun\Desktop\Capture.png";
+
+            var Listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp
+            );
+
+
+
+
+
+            var IP = IPAddress.Parse("127.0.0.1");   
+            var Port = 4000;                         
+
+            var EndPoint = new IPEndPoint(IP, Port); 
+
+            var msg = "";
+            var len = 0;
+            var buffer = new byte[ushort.MaxValue - 29];
+
+            Listener.Bind(EndPoint);
+
+            EndPoint remote_EndPoint = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                len = Listener.ReceiveFrom(buffer, ref remote_EndPoint);
+
+                msg = Encoding.Default.GetString(buffer, 0, len);
+
+                if (msg == "Send Screenshoot")
+                {
+                    Console.WriteLine($"{remote_EndPoint.ToString()}: {msg}");
+
+                    CaptureMyScreen();
+                    
+                    byte[] send_buffer = Encoding.Default.GetBytes(Path);
+                    
+                    Listener.SendTo(send_buffer, remote_EndPoint);
+
+                    Console.WriteLine("Has Been Sent !");
+
+                }
+            }
+
+        }
+
+        static private void CaptureMyScreen()
+        {
+            try
+            {
+                Bitmap Capture_bitmap = new Bitmap(1024, 768, PixelFormat.Format32bppArgb);
+
+                Rectangle Capture_rectangle = Screen.AllScreens[0].Bounds;
+
+                Graphics Capture_graphics = Graphics.FromImage(Capture_bitmap);
+                Capture_graphics.CopyFromScreen(Capture_rectangle.Left, Capture_rectangle.Top, 0, 0, Capture_rectangle.Size);
+
+                Capture_bitmap.Save(@"C:\Users\Elgun\Desktop\Capture.png", ImageFormat.Png);
+
+                Console.WriteLine("Has Been Captured Successfully !");
+             
+            }
+            
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
